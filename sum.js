4d9d059b853ddba53cffcebb4f6058c9e3d8f5b1
jillmfee25 <lisa200325@gmail.com>
2022-05-08 15:26:56 +0800

    sum.js

--- sum.js
+++ sum.js
@@ -2,12 +2,22 @@
 // 回傳 1 + 2 + ... + n 的結果
 function sum1(n) {
     // return 結果
-    let sum = 0;
+    let sum1 = 0;
 for (let i=1; i<=n ;i++){
-    sum += i;
+    sum1 += i;
 }
     return sum;
 }
+
+function sum2(n) {
+    return ((n+1)*n)/2;   
+}
+
+function sum3(n){
+    [1,2,3,...n].reduce((sum,item) => sum + item ,0);
+}
+
+
 
 
   console.log(sum(1)); // 1
