2d41082362c0dfe19d1348d8e163cf2b76392e55
jillmfee25 <lisa200325@gmail.com>
2022-05-14 15:21:01 +0800

    update sum

--- basic/sum.js
+++ basic/sum.js
@@ -1,29 +1,37 @@
-
 // 回傳 1 + 2 + ... + n 的結果
-function sum1(n) {
-    // return 結果
-    let sum1 = 0;
-for (let i=1; i<=n ;i++){
-    sum1 += i;
-}
-    return sum;
+//(變數的名稱)
+function total(10) {
+  // return 結果
+  let result = 0;
+  for (let i = 1; i <= n; i++) {
+    // result += i;
+    result = result + i;
+  }
+  return result;
 }
 
 function sum2(n) {
-    return ((n+1)*n)/2;   
+  return ((n + 1) * n) / 2;
+}
+// function a(n) {
+//   return n;
+// }
+
+function func(sum, item){
+  return sum + item
 }
 
-function sum3(n){
-    [1,2,3,...n].reduce((sum,item) => sum + item ,0);
+
+function sum3(n) {
+  [1, 2, 3, ...n].reduce((sum, item) => sum + item, 0);
 }
 
+console.log(total(100));
 
-
-
-  console.log(sum(1)); // 1
-  console.log(sum(2)); // 3
-  console.log(sum(10)); // 55
-  console.log(sum(100)); // 5050
+// console.log(sum1(1)); // 1
+// console.log(sum1(2)); // 3
+// console.log(sum1(10)); // 55
+// console.log(sum1(100)); // 5050
 
 // for-loop
 // 公式解
